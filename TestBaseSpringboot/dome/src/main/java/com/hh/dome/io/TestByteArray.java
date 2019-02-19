8c033afe6768ef4058b4516759d3431caf0cd8ca
chenguoku <chenguoku@xforceplus.com>
2019-02-19 17:55:30 +0800

    测试 ByteArrayInputStream

---
+++ TestBaseSpringboot/dome/src/main/java/com/hh/dome/io/TestByteArray.java
@@ -0,0 +1,69 @@
+package com.hh.dome.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import org.junit.Test;
+
+import com.hh.dome.entity.User;
+
+public class TestByteArray {
+
+	@Test
+	public void testOne() throws ClassNotFoundException {
+
+		User user = new User("zhangsan",18);
+		
+		ByteArrayOutputStream bos = null;
+		ObjectOutputStream oos = null;
+		ByteArrayInputStream bis = null;
+		ObjectInputStream ois = null;
+		try {
+			bos = new ByteArrayOutputStream();
+			oos = new ObjectOutputStream(bos);
+			oos.writeObject(user);
+
+			bis = new ByteArrayInputStream(bos.toByteArray());
+			ois = new ObjectInputStream(bis);
+			User copy = (User)ois.readObject();
+			
+			System.out.println(user == copy);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (ois != null) {
+				try {
+					ois.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (bis != null) {
+				try {
+					bis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (oos != null) {
+				try {
+					oos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (bos != null) {
+				try {
+					bos.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+}
