370dac221567822d16ea629e903ab60ba959f10c
chenguoku <chenguoku@xforceplus.com>
2019-02-22 18:00:13 +0800

    MD5

--- TestBaseSpringboot/dome/src/test/java/com/hh/dome/DomeApplicationTests.java
+++ TestBaseSpringboot/dome/src/test/java/com/hh/dome/DomeApplicationTests.java
@@ -1,11 +1,17 @@
 package com.hh.dome;
 
-import com.hh.dome.async.Task;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.hh.dome.async.Task;
+import sun.misc.BASE64Encoder;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,6 +34,27 @@ public class DomeApplicationTests {
 
 
 	}
+    
+    /**
+     * MD5 加密
+     */
+    @Test
+    public void testMD5() {
+    	String str = "123456";
+    	String newstr= null;
+        try {
+            //确定计算方法
+            MessageDigest md5=MessageDigest.getInstance("MD5");
+            BASE64Encoder base64en = new BASE64Encoder();
+            //加密后的字符串
+            newstr = base64en.encode(md5.digest(str.getBytes("utf-8")));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println(newstr);
+    }
 
 }
 
