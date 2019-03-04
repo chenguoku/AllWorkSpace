fa6987f26dea6aedd69dd5f87cc1fb5ef146cb0b
chenguoku <chenguoku@xforceplus.com>
2019-03-04 09:47:40 +0800

    整理忽略文件

--- TestBaseSpringboot/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,13 +0,0 @@
-
-e
-5dome/src/test/java/com/hh/dome/lambda/Steam1Test.java,3\a\3af1c41382917344cb32ee899b31deac47f47dcb
-<
-dome/pom.xml,7\1\7164f1c9263af8df034b26b37ec6f9a30a0e9727
-o
-?dome/src/main/java/com/hh/dome/lambda/LambdaAndCollections.java,d\8\d8d671eaf7f0ccd08e6b75f5f8adf56137975d23
-a
-1dome/src/main/java/com/hh/dome/lambda/Steam1.java,4\b\4b3245c9cd89ba9a37b84539bf7db73c43e3252f
-b
-2dome/src/main/java/com/hh/dome/lambda/Stream2.java,3\0\3058086aae1ca6780867db634c35d5090d176d7a
-c
-3dome/src/main/java/com/hh/dome/DomeApplication.java,4\3\435817e545be2bf028ecb3ca9ee08e093a7b9913
---
+++ TestBaseSpringboot/dome/src/main/java/com/hh/dome/sort/QuickSort.java
@@ -0,0 +1,58 @@
+package com.hh.dome.sort;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.xpath;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+/**
+ * 快速排序
+ * @author chenguoku
+ *
+ */
+public class QuickSort {
+
+	public static void quickSort(int[] arr){
+	    qsort(arr, 0, arr.length-1);
+	}
+	private static void qsort(int[] arr, int low, int high){
+	    if (low < high){
+	        int pivot=partition(arr, low, high);        //将数组分为两部分
+	        qsort(arr, low, pivot-1);                   //递归排序左子数组
+	        qsort(arr, pivot+1, high);                  //递归排序右子数组
+	    }
+	}
+	private static int partition(int[] arr, int low, int high){
+	    int pivot = arr[low];     //枢轴记录
+	    while (low<high){
+	        while (low<high && arr[high]>=pivot) --high;
+	        arr[low]=arr[high];             //交换比枢轴小的记录到左端
+	        while (low<high && arr[low]<=pivot) ++low;
+	        arr[high] = arr[low];           //交换比枢轴大的记录到右端
+	    }
+	    //扫描完成，枢轴到位
+	    arr[low] = pivot;
+	    //返回的是枢轴的位置
+	    return low;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {3,8,2,5,7};
+		quickSort(arr);
+		IntStream stream = Arrays.stream(arr);
+		stream.forEach(System.out::print);
+	}
+
+	@Test
+	public void test() {
+		int i = 5;
+		while(i>0) --i;
+		System.out.println(i);
+	}
+	
+}
