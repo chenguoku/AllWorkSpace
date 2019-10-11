184c36dcb7f007c7d945fd8dd51cbaa7fbd7d2f7
chenguoku <cgkhh1996@163.com>
2019-10-12 00:48:44 +0800

    注册的service

--- pet/src/main/java/com/chaoku/modules/app/service/impl/UserServiceImpl.java
+++ pet/src/main/java/com/chaoku/modules/app/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.chaoku.modules.app.service.impl;
 
+import cn.hutool.core.lang.UUID;
 import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -8,18 +9,12 @@ import com.chaoku.common.utils.*;
 import com.chaoku.modules.app.dao.PetDao;
 import com.chaoku.modules.app.dao.UserDao;
 import com.chaoku.modules.app.entity.UserEntity;
-import com.chaoku.modules.app.dto.user.LoginForm;
 import com.chaoku.modules.app.service.UserService;
-import com.chaoku.modules.app.vo.pet.PetVo;
-import com.chaoku.modules.app.vo.user.LoginVo;
-import com.chaoku.modules.app.vo.user.TokenMappingVo;
-import com.chaoku.modules.app.vo.user.UserVo;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -82,17 +77,55 @@ public class UserServiceImpl extends ServiceImpl<UserDao, UserEntity> implements
      */
     @Override
     public Result register(String userInfo) {
-
+        UserEntity userEntity = new UserEntity();
+        Long id = IdGenerator.defaultSnowflakeId();
+        userEntity.setId(id);
+        userEntity.setPetCurrency(0);
+        userEntity.setDiamond(0);
         if (StringUtils.equals(GUEST, userInfo)) {
             // 游客
-            UserEntity userEntity = new UserEntity();
-            boolean save = this.save(userEntity);
+            userEntity.setNickName(UUID.fastUUID().toString().split("-")[0]);
 
         } else {
             //授权用户
             Map map = JSON.parseObject(userInfo, Map.class);
+            if (map.get("nickName") != null) {
+                userEntity.setNickName(String.valueOf(map.get("nickName")));
+            }
+            if (map.get("gender") != null) {
+                userEntity.setGender(Integer.parseInt(String.valueOf(map.get("gender"))));
+            }
+            if (map.get("language") != null) {
+                userEntity.setLanguage(String.valueOf(map.get("language")));
+            }
+            if (map.get("city") != null) {
+                userEntity.setCity(String.valueOf(map.get("city")));
+            }
+            if (map.get("province") != null) {
+                userEntity.setProvince(String.valueOf(map.get("province")));
+            }
+            if (map.get("country") != null) {
+                userEntity.setCountry(String.valueOf(map.get("country")));
+            }
+            if (map.get("avatarUrl") != null) {
+                userEntity.setAvatarUrl(String.valueOf(map.get("avatarUrl")));
+            }
+            if (map.get("unionId") != null) {
+                userEntity.setUnionId(String.valueOf(map.get("unionId")));
+            }
+            if (map.get("watermark") != null) {
+                Map watermark = (Map) map.get("watermark");
+                if (watermark.get("timestamp") != null) {
+                    userEntity.setTimestamp(Long.parseLong(String.valueOf(watermark.get("timestamp"))));
+                }
+                if (watermark.get("appid") != null) {
+                    userEntity.setAppid(String.valueOf(watermark.get("appid")));
+                }
+            }
         }
 
-        return null;
+        boolean save = this.save(userEntity);
+        return new Result().ok(save);
     }
+
 }
