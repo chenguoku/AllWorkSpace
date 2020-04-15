2933d1b22e03d40f452a7d5671e60e0e0c090963
chenguoku <cgkhh1996@163.com>
2020-04-15 11:38:47 +0800

    update README.md

---
+++ TestBaseSpringboot/dome/src/main/java/com/hh/dome/FactoeyDesign/Payment.java
@@ -0,0 +1,12 @@
+package com.hh.dome.FactoeyDesign;
+
+/**
+ * @author chenguoku
+ * @version 1.0.0
+ * @ClassName Payment.java
+ * @Description TODO
+ * @createTime 2020年02月26日 19:38:00
+ */
+public abstract class Payment {
+    public abstract Boolean pay();
+}
